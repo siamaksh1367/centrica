8b2c9c6782057a7e7c52af5d057a4dd554b71788
Siamak Shamaee <sish@healthomat.org>
2023-06-12 16:56:41 +0200

    add proper handling

--- centrica-server/centrica.api/Controllers/DistrictController.cs
+++ centrica-server/centrica.api/Controllers/DistrictController.cs
@@ -3,7 +3,6 @@ using centrica.services.Commands;
 using centrica.services.Queries;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
-using Newtonsoft.Json;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -30,9 +29,7 @@ namespace centrica.api.Controllers
         [HttpPost]
         public async Task Post([FromBody] string value)
         {
-            var json = JsonConvert.DeserializeObject(value);
-            var district = JsonConvert.DeserializeObject<AddDistrictCommand>(json.ToString());
-            await _mediator.Send(district);
+            await _mediator.Send(value.ParseJsonToObject<AddDistrictCommand>());
         }
     }
 }
---
+++ centrica-server/centrica.api/RequestBodyExtensions.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace centrica.api
+{
+
+    public static class RequestBodyExtensions
+    {
+        public static T ParseJsonToObject<T>(this string value)
+        {
+            try
+            {
+                var json = JsonConvert.DeserializeObject(value);
+                var obj = JsonConvert.DeserializeObject<T>(json.ToString());
+                return obj;
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("payload is not convertable to the desired type", e);
+            }
+        }
+    }
+}
